450989cf98465a7ee6005bf192fb5d6ecd46e549
Felipe Besson <flpbesson@gmail.com>
2014-11-02 21:10:01 -0200

    attempt to crawl google jobs

---
+++ jobful/spiders/google-jobs.py
@@ -0,0 +1,43 @@
+from scrapy.contrib.spiders import CrawlSpider
+from scrapy.http import Request
+from scrapy.selector import Selector
+from jobful.items import Job
+from datetime import date
+from selenium import webdriver
+import time
+
+
+class GoogleJobsSpider(CrawlSpider):
+    name = "google-jobs"
+    allowed_domains = ["google.com"]
+    start_urls = ["https://www.google.com/about/careers"]
+
+    def parse(self, response):
+        driver = webdriver.PhantomJS()
+        page_prefix = "https://www.google.com/about/careers/search#t=sq&q=j&li=10&st"
+        pages = ["%s=%d" % (page_prefix, i*10) for i in range(0, 10)]
+
+        for page in pages:
+            driver.get(page)
+            urls = driver.find_elements_by_xpath("//a[@itemprop='url']")
+
+            for url in urls:
+                clean_url = str(url.get_attribute("href")).replace("https", "http") 
+                print clean_url
+                yield Request(clean_url, callback=self.parse_link)
+                time.sleep(5)
+
+        driver.quit()
+
+    def parse_link(self, response):
+        sel = Selector(response)
+        job = Job()
+
+        job["title"] = sel.xpath('//span[@itemprop="name title"]//text()')[0].extract()
+        job["description"] = sel.xpath('//div[@class="detail-item"]//text()').extract()
+        job["location"] = sel.xpath('//div[@class="info-text"]//text()')[0].extract()
+        job["company"] = "google.com"
+        job["url"] = response.url
+        job["updated_at"] = date.today().isoformat()
+
+        return job
