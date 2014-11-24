7c61203071deea3a44214d7f8766a5ff4f882f2b
Fabio <folivei@gmail.com>
2014-11-23 22:24:56 -0200

    with sal

--- tblsal/tblSal.py
+++ tblsal/tblSal.py
@@ -1,11 +1,10 @@
-import datetime
-import sys
+from datetime import datetime
 from splinter import Browser
 import codecs
 import pymongo
+import sys
 
-
-def getBaseInformation(filetoopen):
+def getBrasilInformation(filetoopen):
     #
     # Load base .txt file into memory + build link to be crawled + get default local in US to find salary
     # File format to open: 
@@ -24,16 +23,16 @@ def getBaseInformation(filetoopen):
             Doc['Professional']['English'] = v_temp[3]
             Doc["SalaryBR"] = {}
             Doc["SalaryBR"]["Font"]=v_temp[1]
-            Doc["SalaryBR"]["Data"]=v_temp[0]
-            Doc["SalaryBR"]["JR"]= v_temp[4]
-            Doc["SalaryBR"]["PL"]= v_temp[5]
-            Doc["SalaryBR"]["SR"]= v_temp[6]
+            Doc["SalaryBR"]["Data"]=datetime.strptime(v_temp[0],'%d/%m/%Y')
+            Doc["SalaryBR"]["JR"]= float(v_temp[4]) * 12
+            Doc["SalaryBR"]["PL"]= float(v_temp[5]) * 12
+            Doc["SalaryBR"]["SR"]= float(v_temp[6]) * 12
             ret.append(Doc)
     
     return ret
 
 
-def getUSSalaryCareer(profession, defaultPlace): 
+def getUSSalaryCareer(profession): 
     # Objetive: Crawls the profession link from careerbuilder of one specific profession
     # Parameters: 
     #   profession: profession that I want crawl salary information
@@ -42,13 +41,17 @@ def getUSSalaryCareer(profession, defaultPlace):
     
     seniority  = ["JR","PL", "SR" ] 
     ret = {}
-
+    ret["SalaryUS"] = {}
     for sen in seniority:
         if (sen=='PL'):
            v_sen = ""
+        elif(sen=='JR'):
+           v_sen = "Junior "
         else:
-           v_sen = sen + " "
-        strLink = "http://salary.careerbuilder.com/" + v_sen  + profession.replace("/"," ").strip() + "/" + defaultPlace 
+           v_sen = "Senior "
+        # Always get US average
+        strLink = "http://salary.careerbuilder.com/" + v_sen  + profession.replace("/"," ").strip() + "/San Francisco"
+
         browser = Browser('phantomjs')
         print strLink
         browser.visit(strLink)
@@ -57,26 +60,52 @@ def getUSSalaryCareer(profession, defaultPlace):
         if  browser.is_element_present_by_css('div.salary-average', wait_time=15):
             attributes = browser.find_by_css("div.salary-ajax")
             # it should return one attribute, for "just in case"
+            
             for attribute in attributes:
                # should create one object by link
                x = ExtractSal(profession, strLink, attribute) 
-            if (sen == 'JR'): 
-               ret["SalaryUS"] = {}   
-               ret["SalaryUS"]['JR'] = {}
-               ret["SalaryUS"]['PL'] = {}
-               ret["SalaryUS"]['SR'] = {}
+            
                ret["SalaryUS"]["Font"]= "CareerBuilder"
-               ret["SalaryUS"]["Data"]= datetime.datetime.now()
-               ret["SalaryUS"]["relac"] = x["relac"]
-                    
-            ret["SalaryUS"][sen][defaultPlace] = x["salAverageDefault"]
-            ret["SalaryUS"][sen]["AverageUS"]  = x["salAverageUS"] 
-            ret["SalaryUS"][sen]["link"]= strLink
+               ret["SalaryUS"]["Data"]= datetime.now()
+ 
+            ret["SalaryUS"][sen] = x["salAverageUS"] 
                
         else:
               # to be improved: log error, probably site structure changed
               print "nao achou"        
         browser.quit()
+    
+    return ret
+
+def getUSSalaryCareer(profession, defaultPlace): 
+
+    
+    ret = {}
+    ret["SalaryUS"] = {}
+    strLink = "http://salary.careerbuilder.com/"+ profession.replace("/"," ").strip() + "/" + defaultPlace.strip() + "/" 
+    browser = Browser('phantomjs')
+    print strLink
+    browser.visit(strLink)
+        
+    #look for salary-average tag 
+    if  browser.is_element_present_by_css('div.salary-average', wait_time=15):
+        attributes = browser.find_by_css("div.salary-ajax")
+        # it should return one attribute, for "just in case"
+            
+        for attribute in attributes:
+            # should create one object by link
+            x = ExtractSal(profession, strLink, attribute) 
+        ret["SalaryUS"]["profession"]= profession
+        ret["SalaryUS"]["location"]= defaultPlace
+        ret["SalaryUS"]["Font"]= "CareerBuilder"
+        ret["SalaryUS"]["Data"]= datetime.now()
+        ret["SalaryUS"]['US'] = x["salAverageUS"] 
+        ret["SalaryUS"][defaultPlace] = x["salAverageDefault"]
+               
+    else:
+        # to be improved: log error, probably site structure changed
+        print "nao achou"        
+    browser.quit()
     
     return ret
 
@@ -96,16 +125,12 @@ def ExtractSal(profession, link, attribute):
     # First-Line Supervisors of Retail Sales Workers
     # Network and Computer Systems Administrators  
     ret = {}
-    print "###"
-    print attribute.value; 
-    print "###"
     v_temp = attribute.value.split(' ') # split based on space
-    print len (v_temp)
     # When there's no enough data, site shows only US average, v_temp == 1
     v_salAverageDefault = v_temp[0].replace(",","").replace("$","") #got first word that should be value in default location
     ret["salAverageDefault"] =  v_salAverageDefault
-    if (len(v_temp) > 1):
-        #print v_temp
+    # when there's no enough data, structure of information is different
+    if (len(v_temp) > 10):
         v_percent =  v_temp[3].replace("%","")
         v_qualifier = v_temp[4]
         v_temp = attribute.value.split('\n')
@@ -115,18 +140,38 @@ def ExtractSal(profession, link, attribute):
              salAverageUS = float(v_salAverageDefault) * (1-(float(v_percent)/100))
         elif (v_qualifier == 'below'):
              salAverageUS = float(v_salAverageDefault) * (1+(float(v_percent)/100))
+        else:
+             salAverageUS = 0
         ret["salAverageUS"] = salAverageUS
         ret["relac"] = v_arrayRelac
     else:
-        ret["salAverageUS"] = 0
+        ret["salAverageUS"] = v_salAverageDefault
         ret["relac"] = []   
     return ret
 
-def Persist(doc):
-    # get a handle to the school database
+def PersistSalBR_US(doc):
+    #
+    # persist comparation table in mongoDb
+    #
     connection = pymongo.Connection("mongodb://localhost", safe=True)
     db=connection.jobful
-    tabSal = db.TabSal
+    tabSal = db.salBR_US
+
+
+    #try:
+    tabSal.insert(doc)   # first insert
+
+
+    #except:
+    #    print "Unexpected error:", sys.exc_info()[0]
+
+def PersistJobsSal(doc):
+    #
+    # persist comparation table in mongoDb
+    #
+    connection = pymongo.Connection("mongodb://localhost", safe=True)
+    db=connection.jobful
+    tabSal = db.jobsSal
 
 
     #try:
@@ -137,20 +182,40 @@ def Persist(doc):
     #    print "Unexpected error:", sys.exc_info()[0]
 
 
-def main():
-    start = datetime.datetime.now()
-    # argv1 - salary table in Brazil
-    # argv2 - default place to check value in US
-    TabelaSalarialBase = getBaseInformation(sys.argv[1])
+def BrasilUSSalaryTable(filetoOpen, DefaultPlaceUS):
+    # create salary table Brasil x US
+    start = datetime.now()
+
     TabelaSalarial = []
-    DefaultPlaceUS = sys.argv[2]
     for item in TabelaSalarialBase: 
         ret = getUSSalaryCareer (item["Professional"]["English"], DefaultPlaceUS )
         itemcomplete = dict(list(item.items()) + list(ret.items()))
-        Persist (itemcomplete)
+        PersistSalBR_US (itemcomplete)
         TabelaSalarial.append(itemcomplete)
-    end = datetime.datetime.now()
+        print itemcomplete
+    end = datetime.now()
     print end-start
+
+def USSalaryTable():
+    # create a table with average US Salary based on jobs
+    connection = pymongo.Connection("mongodb://localhost", safe=True)
+    db=connection.jobful
+
+    iter = db.jobs.aggregate([{"$group":{"_id":{"title":"$title","location":"$location"}, "n":{"$sum":"1"}}}]);
+    for doc in iter['result']:
+        print doc['_id']['title'];
+        print doc['_id']['location'];
+        item = getUSSalaryCareer(doc['_id']['title'], doc['_id']['location'])
+        print item
+        PersistJobsSal(item)
+
+
+def main():
+    if (sys.argv[1] == "jobs"):
+        USSalaryTable()
+    else:
+        BrasilUSSalaryTable(sys.argv[1], sys.argv[2])
+
     
 if __name__ == '__main__':
     main()
