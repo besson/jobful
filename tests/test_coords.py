566840c72f09801f7ada80451c22d4dc4b736e99
Fabio <folivei@gmail.com>
2014-12-22 16:57:57 -0200

    jobgeo with corrections

--- tests/test_coords.py
+++ tests/test_coords.py
@@ -1,9 +1,10 @@
 import unittest
+from unittest import TestCase
 import imp
-geo = imp.load_source('geo', '../utils/geo.py')
+geo = imp.load_source('geo', '../jobful/utils/geo.py')
 
-class Test_coords(unittest.TestCase):
-    def test_address(self):
+class TestGetCoordinates(TestCase):
+    def test_valid_address(self):
         addresses = ["850 Cherry Avenue, San Bruno", "WALMART OFFICE SAN BRUNO"]
         for element in addresses:
             ret = geo.coord(element)
