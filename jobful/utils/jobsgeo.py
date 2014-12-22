1ad936252d9ea5dd55bda092dfece05bf764089c
Fabio <folivei@gmail.com>
2014-12-22 15:14:32 -0200

    jobsgeo with corrections

--- jobful/utils/jobsgeo.py
+++ jobful/utils/jobsgeo.py
@@ -3,35 +3,35 @@ import pymongo
 
 import imp
 import geo
-mongo_connector = imp.load_source('x', '../utils/mongo_connector.py')
+mongo_connector = imp.load_source('mc', '../utils/mongo_connector.py')
 
 db=mongo_connector.get_db()
 
-def persistgeo(key, item):
-    ccoord = db.ccoord  
+def persist_company_coordinates(key, item):
+    company_coordinates = db.company_coordinates  
     body = dict(key.items() + item.items())
-    ccoord.update(key, body, upsert=True)   
+    company_coordinates.update(key, body, upsert=True)   
   
-def persistNF(doc):
-    ccoordnf= db.ccoordnf
-    ccoordnf.update(doc, doc,upsert=True) 
+def persist_coordinates_not_found(doc):
+    coordinates_not_found= db.coordinates_not_found
+    coordinates_not_found.update(doc, doc,upsert=True) 
 
-def getCompanies():
+def get_coord_companies():
     iter = db.jobs.aggregate([{"$group":{"_id":{"company":"$company","location":"$location"}}}]);
     for doc in iter['result']:
         try:
 
             coords = geo.coord(doc['_id']['company'] + " " + doc['_id']['location']) 
             if coords is None:
-               persistNF(doc)
+               persist_coordinates_not_found(doc)
             else:
-               persistgeo(doc,coords)
+               persist_company_coordinates(doc,coords)
         except:
             print "Unexpected error:", sys.exc_info()[0] 
             continue
 
 def main():
-        getCompanies()
+    get_coord_companies()
 
 if __name__ == '__main__':
     main()
