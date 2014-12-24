3e8044a7aee33e35e1e4c32fd6be422241ad3bc2
Felipe Besson <flpbesson@gmail.com>
2014-12-23 23:11:49 -0200

    indexing geo location

--- jobful/solr_indexer.py
+++ jobful/solr_indexer.py
@@ -5,13 +5,20 @@ from utils.mongo_connector import get_db
 
 def index():
     s = Solr('http://localhost:8983/solr/jobs')
-    today = date.today().isoformat()
     db = get_db()
 
-    for job in db.jobs.find({"updated_at": today}):
+    for job in db.jobs.find():
         job["id"] = str(job.pop("_id"))
         job["updated_at"] = date.today()
-        print dict(job)
-        s.add(dict(job), commit=True)
+
+        try:
+            geo = db.company_coordinates.find({"$and": [{"_id.company": job["company"]}, {"_id.location": job["location"]}]})[0]
+            job["geo_location"] = "%f,%f" % (geo["lat"], geo["lng"])
+            print dict(job)
+
+            s.add(dict(job), commit=True)
+        except Exception:
+            print "error \n"
+            pass
 
 index()
